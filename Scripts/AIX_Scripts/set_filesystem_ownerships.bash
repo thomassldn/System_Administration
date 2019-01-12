2f38ec96eb1df12df3fd8bd1c7dc89218d18be79
thomassldn <003686838@coyote.csusb.edu>
2019-01-11 16:16:04 -0800

    Update set_filesystem_ownerships.bash

--- Scripts/AIX_Scripts/set_filesystem_ownerships.bash
+++ Scripts/AIX_Scripts/set_filesystem_ownerships.bash
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 #Desc: This script sets the ownerships of filesystems
 
-
-
-chown chIoIdHl:chSsGpHl
+chown -R user:group /qmd/cache_site_1/
+chown -R user:group /qmd/cache_SITE_1/ 

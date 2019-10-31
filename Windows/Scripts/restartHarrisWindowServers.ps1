df7132cab8e768de1e399eaa497f768ede1423e2
thomassldn <003686838@coyote.csusb.edu>
2019-10-31 15:07:31 -0700

    Update restartHarrisWindowServers.ps1

--- Windows/Scripts/restartHarrisWindowServers.ps1
+++ Windows/Scripts/restartHarrisWindowServers.ps1
@@ -1,1 +1,17 @@
+#Author:Thomas S, ATS
+#Date:Oct 31, 2019
+#Desc: This power shell restarts Harris' Window Servers ( "sbqep1", "sbqep2", "sbqmc1", "sbqmc2")
 #
+
+#Restart the "sbqep1", "sbqep2", "sbqmc1", "sbqmc2" servers forcefully. These servers are currently not being used, so 
+#no damage can be done. 
+Restart-Computer -ComputerName "sbqep1", "sbqep2", "sbqmc1", "sbqmc2" -Force
+
+#After restarting the above servers, sleep for 15 seconds before restartign this server. 
+Start-Sleep -s 15
+
+#When this command is executed, you will get logged off this server, and it will reboot. 
+Restart-Computer -ComputerName "sbqima" -Force 
+
+#End
+

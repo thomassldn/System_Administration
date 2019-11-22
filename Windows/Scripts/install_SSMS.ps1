21b85b5c957a5ec82a36c19ab007b56e1df5c9db
thomassldn <003686838@coyote.csusb.edu>
2019-11-21 19:52:15 -0800

    Create install_SSMS.ps1

---
+++ Windows/Scripts/install_SSMS.ps1
@@ -0,0 +1,35 @@
+#Author:Thomas S, ATS
+#Date: 22 Nov 2019
+#Desc: This power shell script downloads SSMS and installs it 
+#
+#Folder where SSMS installer will be downloaded and ran in 
+#Modify ssmsPath below or script will not work
+$ssmsPath="C:\Users\qmsupport\Downloads\HISMC_Installation_Files" 
+
+#SSMS executable 
+$ssmsExecutable="$ssmsPath\SSMS-Setup-ENU.exe"
+
+#Download SSMS executable 
+If (!(Test-Path $ssmsExecutable)){
+    
+    Write-Host "`nDownloading SSMS 18.4..."
+    $URL = "https://aka.ms/ssmsfullsetup"
+
+    $webClient = New-Object System.Net.WebClient
+    $webClient.DownloadFile($url,$ssmsExecutable)
+
+    Write-Host "`nSSMS 18.4 installer download complete ..." -ForegroundColor Green
+
+} 
+
+ 
+#Run SSMS installer
+Write-Host "`nBeginning the SSMS installion..." 
+
+#Create Installation Parameters, log file
+$installationParameters =  "/Install /Quiet /Norestart /Logs SSMS_install_log.txt"
+$args = $installationParameters.Split(" ")
+
+#Execute SSMS Executeable and pass above arguments
+& "$ssmsExecutable" $args 
+Write-Host "`nSSMS installation complete!" -ForegroundColor Green

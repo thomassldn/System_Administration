8ad1345ee697ccda00adf7081c1908a066ba0ffd
thomassldn <003686838@coyote.csusb.edu>
2020-06-01 13:45:44 -0700

    Create install_Roles_Features_ClinDoc.ps1

---
+++ Windows/Scripts/install_Roles_Features_ClinDoc.ps1
@@ -0,0 +1,2 @@
+Install-WindowsFeature -Name NET-Framework-Features,NET-Framework-Core,NET-HTTP-Activation,NET-Framework-45-Features,NET-Framework-45-Core,NET-Framework-45-ASPNET,Web-Server,Web-WebServer,Web-Common-Http,Web-Default-Doc,Web-Dir-Browsing,Web-Http-Errors,Web-Static-Content,Web-Http-Redirect,Web-Http-Logging,Web-Custom-Logging,Web-Stat-Compression,Web-Filtering,Web-Mgmt-Console,Web-Log-Libraries,Web-Asp-Net,Web-ASP,Web-AppInit,Windows-Identity-Foundation -IncludeManagementTools 
+#-Restart
